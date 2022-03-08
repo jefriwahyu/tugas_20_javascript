ef34d7eadbe18191cbb807f8f1652dd83279fd8e
Jefriwahyu <memberjefry1@gmail.com>
2022-03-08 10:45:19 +0700

    Tugas 20 Javascript
    Tugas ini saya kumpulkan untuk NIOMIC.

---
+++ index.js
@@ -0,0 +1,8 @@
+function words(){
+  var paragraph = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+  console.log(paragraph.match("are"));
+  console.log(paragraph.match("makan bang?"));
+  console.log(paragraph.match("words"));
+}
+
+words();
